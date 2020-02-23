3c06887a2a2128ee53b625bc1703ad2d83de8ba2
Andre Prihodko <andreprihodko5@gmail.com>
2020-02-23 11:09:47 +0100

    Add files via upload

--- time4int/mipslabwork.c
+++ time4int/mipslabwork.c
@@ -15,9 +15,12 @@ For copyright and licensing, see file COPYING */
 #include "mipslab.h"  /* Declarations for these labs */
 
 int prime = 1234567;
-int mytime = 0x0000;
+int mytime = 0x5957;
 int timeoutcount = 0;
-char difficulty = 0; 
+
+char lives = 3;
+
+void enableInterrupt(void);
 
 char textstring[] = "text, more text, and even more text!";
 
@@ -26,20 +29,17 @@ volatile int *LED; //Declare *LED as a global pointer variable
 /* Interrupt Service Routine */
 void user_isr( void )	{
 	
-	//Andrej's code Lab 3 
-	
 	if(IFS(0) & 0x100) {
 		IFSCLR(0) = 0x100;
 		timeoutcount++;
 	}
 	
-	//Edward's code Lab 3 
 	if(IFS(0) & 0x080) {
 		*LED = *LED + 1;
 		IFSCLR(0) = 0x080;
 	}
 	
-	//Lab 3 
+	
 	if(timeoutcount == 10) {
 		time2string( textstring, mytime );
 		display_string( 3, textstring );
@@ -52,20 +52,15 @@ void user_isr( void )	{
 /* Lab-specific initialization goes here */
 void labinit( void )
 {
-	
-	//Andrej's code 
 	LED = (volatile int *) 0xbf886110; //Set LED address equal to the PORTE address
 	*LED &= 0x0ff; //Mask the last byte 
 	
-	//Edward's code 
 	TRISD = 0xFE0;
 	PR2 = ((80000000 / 256) / 10);
 	TMR2 = 0;
 
 	IEC(0) = 0x100;
 	
-	
-	//Andrej's code 
 	PORTE = 0;
 	IEC(0) = 0x80;
 	IPC(1) = 0xF000000;
@@ -81,42 +76,59 @@ void labinit( void )
 
 /* This function is called repetitively from the main program */
 void labwork( void ) {
-	
-//Given 
  prime = nextprime( prime );
  display_string( 0, itoaconv( prime ) );
  display_update();
  
- 
-//Edward Leander - Controlling difficulty based upon the switches
-//Furthest left = Easy, Furthest right = Hard 
+ int btnValue;
+  int swValue;
+  btnValue = getbtns();
+  if(btnValue != 0) {
+	if(btnValue & 0x1) { //BTN2
+		swValue = getsw();
+		swValue = swValue << 4;
+		mytime = (mytime & ~0x0f0) + (swValue & 0x0ff);
+	}
+	if(btnValue & 0x2) { //BTN3
+		swValue = getsw();
+		swValue = swValue << 8;
+		mytime = (mytime & ~0x0f00) + (swValue & 0x0f00);
+	}
+	if(btnValue & 0x4) { //BTN4
+		swValue = getsw();
+		swValue = swValue << 12;
+		mytime = (mytime & ~0x0f000) + (swValue & 0x0f000);
+	}		
+  }
+}
 
+//Andrejs Prihodjko
+void displayLives(char lives) {
+	
+	//Check number of lives and turn on LEDs accordingly
+	switch(lives) {
+		case 3:
+			*LED = 0x4;	//Three lights on
+			break;
+		case 2:
+			*LED = 0x2; //Two lights on
+			break;
+		case 1:
+			*LED = 0x1;	//One light on
+			break;
+		case 0:
+			*LED = 0x0;	//No lights on
+			break;
+		default:
+			break;
+	}
+	
+}
+//BTN2 = 001
+//BTN3 = 010
+//BTN4 = 100
 
 //SW1 = 0001
 //SW2 = 0010
 //SW3 = 0100
 //SW4 = 1000
- 
-int swValue1,swValue2, swValue2;
-if (difficulty = 0) {
-//SW4, Furthest left switch
-		swValue1 = getsw();
-		swValue1 = swValue << 12;
-		if (swValue1 = 1)
-			difficulty = 1;
-
-//SW3, Middle switch
-		swValue2 = getsw();
-		swValue2 = swValue << 8;
-		if (swValue2 = 1)
-			difficulty = 2; 
-
-
-//SW2, Furthest right Switch
-		swValue3 = getsw();
-		swValue3 = swValue << 4;
-		if (swValue2 = 1)
-			difficulty = 3; 
-}
-}
-
